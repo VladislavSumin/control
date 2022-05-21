9582d8a972fc4cdcf0db64f6688ec6b2fe16565b
Vladislav Sumin <vladislavsumin@mail.ru>
2022-05-21 20:24:51 +0300

    [server] add module && add kotlin plugin

--- build-script/build.gradle.kts
+++ build-script/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     // TODO подождать пока эта фича появится в гредле
     // а пока костыль вот отсюда https://github.com/gradle/gradle/issues/15383
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+    implementation(libs.gradlePlugins.kotlin.core)
 }
 
 java {
