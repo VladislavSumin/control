e549cd7a9b366310d3eb7dab763747fc85f200a0
Vladislav Sumin <vladislavsumin@mail.ru>
2022-06-13 11:00:55 +0300

    [server] enable coroutines debug

--- server/src/jvmMain/kotlin/ru/vs/control/server/Main.kt
+++ server/src/jvmMain/kotlin/ru/vs/control/server/Main.kt
@@ -2,6 +2,7 @@ package ru.vs.control.server
 
 import co.touchlab.kermit.Logger
 import io.sentry.Sentry
+import kotlinx.coroutines.DEBUG_PROPERTY_NAME
 import kotlinx.coroutines.launch
 import org.kodein.di.direct
 import org.kodein.di.instance
@@ -16,9 +17,13 @@ fun main() {
 
     Logger.i("Starting server")
 
+    // Enable coroutine debug
+    System.setProperty(DEBUG_PROPERTY_NAME, "on")
+
     Sentry.init { options ->
         options.dsn = "https://2bad5747065c4d64b7cdfa70c64f80fc@o512687.ingest.sentry.io/6495152"
         options.tracesSampleRate = 1.0
+        options.isPrintUncaughtStackTrace = true
     }
 
     // Create server scope
